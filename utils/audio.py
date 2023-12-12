0879ea2137ec97057ebf5b4dad353d52014c57aa
gunwoong <gwj0421@gmail.com>
2023-12-12 18:31:14 +0900

    feat : 디버깅을 위한 Audio display 기능 추가

--- utils/audio.py
+++ utils/audio.py
@@ -1,15 +1,23 @@
 from scipy.io.wavfile import write
+from IPython.display import Audio, display
+
 import io
 
 class AudioUtils:
     @staticmethod
-    def getListFromData(inputs,identifier):
-        return inputs.split(identifier)
-
-    @staticmethod
     def convert_numpy2bytes(numpy_array, sample_rate=44100):
-        # WAV 파일로 저장하지 않고 NumPy 배열을 바이트로 변환
+        # numpy to wav ByteIo
         wav_file = io.BytesIO()
         write(wav_file, sample_rate, numpy_array)
         wav_bytes = wav_file.getvalue()
         return wav_bytes
+
+    @staticmethod
+    def save_audio(audio_values,sampling_rate):
+        for i in range(audio_values.shape[0]):
+            write(f"test_{i}.wav", rate=sampling_rate, data=audio_values[i, 0].numpy())
+
+    @staticmethod
+    def display_audio(audio_values,sampling_rate):
+        for i in range(audio_values.shape[0]):
+            display(Audio(audio_values[i,0].numpy(), rate=sampling_rate))
