a5afcb7de0341eb41fb4e9eb59079a97888e911c
gunwoong <gwj0421@gmail.com>
2023-12-12 18:41:54 +0900

    fix : 유튜브 영상 다운 기능 제거
    해당 모델은 음성 입력에 제한이 많았다.
    
    모델이 생성할 수 있는 음성 길이는 30초 정도됨으로 벨소리를 제작하는 과정에서 문제는 없었다.
    
    하지만, 오디오를 입력하는 경우 입력된 오디오는 20초를 넘기지 못하고 만들 수 있는 오디오는 10초를 넘기지 못한다.
    
    벨소리 생성이라는 프로젝트 목적상 10초는 너무 짧아 해당 기능은 제거하였다.
    
    참고 : https://huggingface.co/docs/transformers/model_doc/musicgen#unconditional-generation

--- service/youtube.py
+++ service/youtube.py
@@ -1,23 +1,23 @@
-from configuration.constant import LOCAL_YOUTUBE_SAVE_ROOT_PATH
-import pytube
-import os
-import librosa
-
-
-def download_youtube_audio(url):
-    # YouTube 동영상을 가져옵니다.
-    video = pytube.YouTube(url)
-
-    # 음성 스트림을 가져옵니다.
-    audio_stream = video.streams.filter(only_audio=True).first()
-    filename = f"{video.title}.wav"
-    file_path = os.path.join(LOCAL_YOUTUBE_SAVE_ROOT_PATH, filename)
-
-    if not os.path.exists(file_path):
-        # 음성 스트림을 다운로드합니다.
-        audio_stream.download(LOCAL_YOUTUBE_SAVE_ROOT_PATH,filename)
-    return file_path
-
-def get_audio_from_youtube_uri(url):
-    y, _ = librosa.load(download_youtube_audio(url))
-    return y
+# from configuration.constant import LOCAL_YOUTUBE_SAVE_ROOT_PATH
+# import pytube
+# import os
+# import librosa
+#
+#
+# def download_youtube_audio(url):
+#     # YouTube 동영상을 가져옵니다.
+#     video = pytube.YouTube(url)
+#
+#     # 음성 스트림을 가져옵니다.
+#     audio_stream = video.streams.filter(only_audio=True).first()
+#     filename = f"{video.title}.wav"
+#     file_path = os.path.join(LOCAL_YOUTUBE_SAVE_ROOT_PATH, filename)
+#
+#     if not os.path.exists(file_path):
+#         # 음성 스트림을 다운로드합니다.
+#         audio_stream.download(LOCAL_YOUTUBE_SAVE_ROOT_PATH,filename)
+#     return file_path
+#
+# def get_audio_from_youtube_uri(url):
+#     y, _ = librosa.load(download_youtube_audio(url))
+#     return y
